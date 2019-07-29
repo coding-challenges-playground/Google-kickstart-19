12d85b685c8128f15a189780fa936f9f6ab417b0
Nikhil Sharma <30630904+nik72619c@users.noreply.github.com>
2019-07-29 19:12:36 +0530

    Update WiggleWalk.js

--- Round-C/WiggleWalk.js
+++ Round-C/WiggleWalk.js
@@ -1,10 +1,10 @@
-var instructionSet='EEWNSNS';
+var instructionSet='NEESSWWNESE';
 var position= {
-  i: 1, j: 2
+  i: 2, j: 3
 };
 
-var rowCount=3;
-var columnCount= 6;
+var rowCount=5;
+var columnCount= 8;
 
 var matrix= prepareMatrix(rowCount, columnCount);
 matrix[position.i][position.j]=1;
@@ -19,18 +19,25 @@ function moveNorth(){
     position.i--;
     matrix[position.i][position.j]=1;
   }
+//   console.log('after moving north the new position is ');
+//   console.log('row ', position.i+1);
+//   console.log('column', position.j+1);
     
 }
 
 function moveSouth(){
-  if(position.i!==(rowCount-1)){
+  if(position.i!==(rowCount)){
     position.i++;
   matrix[position.i][position.j]=1;
   }
-  while(matrix[position.i+1][position.j]===1 && position.i!==(rowCount)){
+  while(matrix[position.i][position.j]===1 && position.i!==(rowCount)){
     position.i++;
-    matrix[position.i][position.j]=1;
+ 
   }
+   // console.log('after moving south the new position is ');
+     matrix[position.i][position.j]=1;
+//   console.log('row ', position.i+1);
+//   console.log('column', position.j+1);
   
 }
 
@@ -43,18 +50,26 @@ function moveEast(){
     position.j++;
     matrix[position.i][position.j]=1;
   }
+//     console.log('after moving east the new position is ');
+//   console.log('row ', position.i+1);
+//   console.log('column', position.j+1);
 }
 
 function moveWest(){
-  if((position.j-1)>=0){
+  if((position.j)>=0){
         position.j--;
   matrix[position.i][position.j]=1;
+   // console.log('called once');
   }
 
-  while((position.j-1)>=0 && matrix[position.i][position.j-1]===1){
+  while((position.j)>=0 && matrix[position.i][position.j]===1){
     position.j--;
-    matrix[position.i][position.j]=1;
+//     console.log('position changed');
   }
+//     console.log('after moving West the new position is ');
+      matrix[position.i][position.j]=1
+//   console.log('row ', position.i+1);
+//   console.log('column', position.j+1);
     
 }
 
@@ -73,7 +88,7 @@ function prepareMatrix(rowCount, columnCount){
 // console.log('matrix obtained is', matrix);
 
 for(let i=0;i<instructionSet.length;i++){
-  console.log('reading instruction '+ instructionSet.charAt(i));
+//   console.log('reading instruction '+ instructionSet.charAt(i));
   switch(instructionSet.charAt(i)){
     case 'N': moveNorth();
       break;
@@ -88,28 +103,3 @@ for(let i=0;i<instructionSet.length;i++){
 
 console.log('row '+(position.i+1));
 console.log('column '+(position.j+1));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
